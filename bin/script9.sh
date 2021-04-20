ee6d693ba4fca87b88f214b03a3d63dd9266e398
Franco <77828917+FSP-1@users.noreply.github.com>
2021-04-20 10:24:03 +0200

    Update script9.sh

--- bin/script9.sh
+++ bin/script9.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 clear
-if [ $# = 1 ] || [ $# = 2 ] ; then
- if test -d $HOME/$1; then
- echo "Es un directorio"
- if test -e $HOME/$1/$2; then
-  echo "Es un fichero"
- file $HOME/$1/$2
+if [ $# = 1 ]; then
+   if test -e $HOME/$1; then
+     if test -d $HOME/$1; then
+      echo "Es un directorio"
+      ls -l $HOME/$1
+    else
+       echo "Es un fichero"
+       more $HOME/$1
+    fi
+  else
+   echo "No existe el directorio o fichero"
+  fi
  else
- echo "No existe el fichero"
- fi
- else
- echo "No existe el directorio"
- fi
-else
- echo "No ha pasado el número correcto de argumentos"
+  echo "No ha pasado el número correcto de argumentos"
 fi
