3fde31a0892f8f0336b57a8e0afee6268228b44c
Franco <77828917+FSP-1@users.noreply.github.com>
2021-04-20 11:41:32 +0200

    Update script10.sh

--- bin/script10.sh
+++ bin/script10.sh
@@ -4,26 +4,33 @@ while [ "$RESPUESTA" != "2" ];
 do  clear
   cat <<-EOF
 MENÚ DE BUSQUEDA DE FICHEROS
-1  Buscar el fichero
+1  Buscar los caracteres del fichero
 2  Salir
 EOF
   read -p "Escoga una opción " RESPUESTA
+  echo
 
   case $RESPUESTA in
-    1)echo "Introduzca los caracteres del fichero que quiere buscar"
+    1)clear
+    echo "Introduzca los caracteres del fichero que quiere buscar"
     read  CARACT
     if [ $CARACT ] 
     then
-    find / ~/ -name "$CARACT*" 
+    echo
+    find  ~/ -name "$CARACT*" 
+    echo
     echo "Busqueda hecha"
+    echo
     read -p 'Pulsa cualquier tecla para continuar ' PAUSA
 else 
     echo "No existe el fichero"
     read -p 'Pulsa cualquier tecla para continuar ' PAUSA
 fi;;
-    2) echo 
-    echo "Adiós y gracias por su visita"
+    2)echo "Adiós y gracias por su visita"
       echo;;
-    *) echo "Debe escoger una opción válida";;
+    *)echo 
+    echo "Debe escoger una opción válida"
+    echo
+    read -p '' PAUSA;;
   esac
 done
