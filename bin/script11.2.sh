8727aa1eefd28e8704a8ffb8913f774c7505df72
Franco <77828917+FSP-1@users.noreply.github.com>
2021-05-05 12:21:30 +0200

    Delete bin directory

--- bin/script11.2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-read -p "Introduzca un numero " a
-for ((i=$a;$i>=0;i--)); do
-echo " $i"
-sleep 0.01
-done
--- bin/script24.sh
+++
@@ -1,5 +0,0 @@
-!#bin/bash
-clear
-read -p "Introduzca el valor A" a
-read -p "Introduzca el valor B" b
-read -p "Introduzca el valor C" c
