8154d91c008d3130ac19a8348ea5d1bca6e54522
Franco <77828917+FSP-1@users.noreply.github.com>
2021-05-05 12:43:24 +0200

    Update script24.sh

--- bin/script24.sh
+++ bin/script24.sh
@@ -8,7 +8,7 @@ echo
 LON=$(expr length "$a")
 PATRON1=""
 for ((i=1;$i<=$LON;i++)); do
-PATRON1=$PATRON1[0-9," "]
+PATRON1=$PATRON1[0-9," ",.,-]
 done
 VAL1=$(expr match "$a" "$PATRON1")
 if [ $VAL1 = $LON ] ;then
@@ -23,7 +23,7 @@ fi
 LON2=$(expr length "$b")
 PATRON2=""
 for ((i=1;$i<=$LON2;i++)); do
-PATRON2=$PATRON2[0-9," "]
+PATRON2=$PATRON2[0-9," ",.,-]
 done
 VAL2=$(expr match "$b" "$PATRON2")
 if [ $VAL2 = $LON2 ] ;then
@@ -38,7 +38,7 @@ fi
 LON3=$(expr length "$c")
 PATRON3=""
 for ((i=1;$i<=$LON3;i++)); do
-PATRON3=$PATRON3[0-9," "]
+PATRON3=$PATRON3[0-9," ",.,-]
 done
 VAL3=$(expr match "$c" "$PATRON3")
 if [ $VAL3 = $LON3 ] ;then
@@ -63,7 +63,8 @@ if [ $VAL1 = $LON ] ;then
   if [ $VAL2 = $LON2 ] ;then
     if [ $VAL3 = $LON3 ] ;then
 r=$[$b*$b-4*$a*$c];
-d=$[ "sqrt($r)" | bc -l | cut -c 1-4 ]
+e=$(echo "sqrt($r)" | bc -l)
+d=$[ $e  | cut -c 1-4 ];
 
 x1=$[-$b + $d / (2*$a) | bc -l ];
 x2=$[-$b - $d / (2*$a) | bc -l ];
@@ -85,5 +86,3 @@ fi
 echo
 read -p 'Pulsa cualquier tecla para acabar ' PAUSA
 clear
-
-
